cee23f53d300118689026e3860580fb72d1bb9bb
czue <czue@localhost>
2024-04-09 12:49:09 +0000

    Pegasus update. Version 2024.4

--- general_balance/settings.py
+++ general_balance/settings.py
@@ -24,7 +24,7 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/stable/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY", default="TCAomzdKyGnfbhCFYrZsBXITkIbUYMdmSyWszZfK")
+SECRET_KEY = env("SECRET_KEY", default="kVejzpzBmgFhUTBdzXPhfBDIwDPbblbsNkLsqyrf")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=True)
